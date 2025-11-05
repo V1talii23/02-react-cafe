e4594f39bcf7c4326ccfe65a99c878ff5ac88094
Vitalii Savchuk <vetalsavchuk23@gmail.com>
2025-11-05 19:18:46 +0000

    change file name

---
+++ src/components/SomeNewApp.tsx
@@ -0,0 +1,52 @@
+import css from "./App.module.css";
+import CafeInfo from "./CafeInfo.tsx";
+import VoteOptions from "./VoteOptions.tsx";
+import VoteStats from "./VoteStats.tsx";
+import Notification from "./Notification.tsx";
+import { useState } from "react";
+import type { Votes, VoteType } from "../types/votes.ts";
+
+const initialVotes = { good: 0, neutral: 0, bad: 0 };
+
+function App() {
+  const [votes, setVotes] = useState<Votes>(initialVotes);
+
+  function handleVote(type: VoteType) {
+    setVotes((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  }
+
+  function resetVotes() {
+    setVotes(initialVotes);
+  }
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
+
+export default App;
