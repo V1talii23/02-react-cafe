a200e519a4895ec4d8c4fd31964b352d27b5b5d6
Vitalii Savchuk <vetalsavchuk23@gmail.com>
2025-11-05 19:45:16 +0000

    move types file into src folder

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import VoteOptions from '../VoteOptions/VoteOptions.tsx';
 import VoteStats from '../VoteStats/VoteStats.tsx';
 import Notification from '../Notification/Notification.tsx';
 import { useState } from 'react';
-import type { Votes, VoteType } from '../types/votes.ts';
+import type { Votes, VoteType } from '../../types/votes.ts';
 
 const initialVotes = { good: 0, neutral: 0, bad: 0 };
 
